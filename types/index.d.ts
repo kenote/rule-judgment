6cc177f58fa1f133a70fe6e9fbe30e28bd54e702
thondery <thondery@163.com>
2020-10-21 19:58:32 +0800

    fix types error.

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
-export function ruleJudgment (query: Partial<any>): (data: any) => boolean
-export function ruleJudgment (query: Partial<any>, options: Partial<any>): (data: any) => boolean
+
 export function parseAssign (data: any, options?: Partial<any>): any
 export function isDateString (value: string): boolean
 export function emit (value: string): any
+
+export default function (query: Partial<any>, options?: Partial<any>): (data: any) => boolean
